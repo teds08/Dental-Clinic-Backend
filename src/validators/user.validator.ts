1389deaf9f83c25406a897afcca6054cb3fb9efc
teds <teddyantepuesto@gmail.com>
2026-09-04 23:44:37 +0800

    add strong password validation rules to registerValidator

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -4,7 +4,17 @@ export const registerValidator = z.object({
   first_name: z.string().min(3),
   last_name: z.string().min(3),
   email: z.email(),
-  password: z.string().min(8),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters.")
+    .max(64, "Password must be less than 64 characters.")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .regex(/[0-9]/, "Password must contain at least one number.")
+    .regex(
+      /[^a-zA-Z0-9]/,
+      "Password must contain at least one special character.",
+    ),
   contact_number: z
     .string()
     .regex(
